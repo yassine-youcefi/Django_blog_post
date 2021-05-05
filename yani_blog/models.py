1db79bb075a521e6cdc192b81ac28f4eaa35e5cb
yani27 <yanilacamora@gmail.com>
2021-05-05 15:43:17 +0100

    create basic user model

--- yani_blog/models.py
+++ yani_blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    age = models.IntegerField(blank=True)
+    birthday = models.DateTimeField('date published', null=True)
--- yani_blog/views.py
+++ yani_blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 # Create your views here.
 
 
