13f4d038422e04be121d5c490e3b70bc682233e1
yani27 <yanilacamora@gmail.com>
2021-05-07 16:19:41 +0100

    custum posts in templates

--- yani_blog/views.py
+++ yani_blog/views.py
@@ -1,11 +1,54 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
-# Create your views here.
+
+posts = [
+
+    {
+        'author': 'yassine youcefi',
+        'title': 'Blog jdid hada 1',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 21'
+    },
+    {
+        'author': 'chaindraa wahid',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 22'
+
+    },
+    {
+        'author': 'habibi fouad',
+        'title': 'Blog for fouad 2',
+        'content': ' first blog for test fouad it is hardcoded',
+        'date_posted': 'Mai 30'
+
+    },
+    {
+        'author': 'ghazali fatima',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 29'
+
+    },
+    {
+        'author': 'chaindraa wahid',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 22'
+
+    }
+]
+
 
 
 def index(request):
-    return render(request, 'templates/index.html')
+    context={
+        'posts' : posts,
+        'title' : 'Home page'
+
+    }
+    return render(request, 'templates/index.html', context)
 
  
 def about(request):
