840f5856905c41f5e5738c1941c491178e6ec46c
yani27 <yanilacamora@gmail.com>
2021-05-07 18:02:55 +0100

    models and get date from there to templates

--- yani_blog/models.py
+++ yani_blog/models.py
@@ -1,13 +1,14 @@
 from django.db import models
-
+from django.utils import timezone
+from django.contrib.auth.models import User 
 # Create your models here.
 
 
-class User(models.Model):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100)
-    age = models.IntegerField(blank=True)
-    birthday = models.DateTimeField('date published', null=True)
-
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_post = models.DateTimeField(default=timezone.now)  
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     def __str__(self):
-        return f'user : {self.username}'
+        return self.title
--- yani_blog/views.py
+++ yani_blog/views.py
@@ -1,82 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import User
+from .models import Post,User
 
-posts = [
 
-    {
-        'author': 'yassine youcefi',
-        'title': 'Blog jdid hada 1',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 21'
-    },
-    {
-        'author': 'chaindraa wahid',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 22'
-
-    },
-    {
-        'author': 'habibi fouad',
-        'title': 'Blog for fouad 2',
-        'content': ' first blog for test fouad it is hardcoded',
-        'date_posted': 'Mai 30'
-
-    },
-    {
-        'author': 'ghazali fatima',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 29'
-
-    },
-    {
-        'author': 'chaindraa wahid',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 22'
-
-    },
-    {
-        'author': 'yassine youcefi',
-        'title': 'Blog jdid hada 1',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 21'
-    },
-    {
-        'author': 'chaindraa wahid',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 22'
-
-    },
-    {
-        'author': 'habibi fouad',
-        'title': 'Blog for fouad 2',
-        'content': ' first blog for test fouad it is hardcoded',
-        'date_posted': 'Mai 30'
-
-    },
-    {
-        'author': 'ghazali fatima',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 29'
-
-    },
-    {
-        'author': 'chaindraa wahid',
-        'title': 'Blog jdid hada 2',
-        'content': 'my first blog for test it is hardcoded',
-        'date_posted': 'April 22'
-
-    }
-]
 
 
 
 def index(request):
+
+    posts = Post.objects.all()
+    print('this is my posts >> \n',posts)
     context={
         'posts' : posts,
         'title' : 'Home page'
