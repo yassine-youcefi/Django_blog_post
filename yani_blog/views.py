020776fcae54dcff3bc9f642f2f3799275e0e8bb
yani27 <yanilacamora@gmail.com>
2021-05-07 17:00:18 +0100

    add static file & layout - sections folders and some templating features

--- yani_blog/views.py
+++ yani_blog/views.py
@@ -37,6 +37,40 @@ posts = [
         'content': 'my first blog for test it is hardcoded',
         'date_posted': 'April 22'
 
+    },
+    {
+        'author': 'yassine youcefi',
+        'title': 'Blog jdid hada 1',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 21'
+    },
+    {
+        'author': 'chaindraa wahid',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 22'
+
+    },
+    {
+        'author': 'habibi fouad',
+        'title': 'Blog for fouad 2',
+        'content': ' first blog for test fouad it is hardcoded',
+        'date_posted': 'Mai 30'
+
+    },
+    {
+        'author': 'ghazali fatima',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 29'
+
+    },
+    {
+        'author': 'chaindraa wahid',
+        'title': 'Blog jdid hada 2',
+        'content': 'my first blog for test it is hardcoded',
+        'date_posted': 'April 22'
+
     }
 ]
 
