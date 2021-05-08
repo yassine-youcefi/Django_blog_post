4452bf6c7b094a17a08bba97bfd0351d25284251
yani27 <yanilacamora@gmail.com>
2021-05-08 03:46:22 +0100

    update signup

--- user/views.py
+++ user/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def sign_up(request):
-    form = UserCreationForm()
+    if request.method == 'POST':    
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account is create successfuly for {username}')
+            return redirect('index')
+    else:
+            form = UserCreationForm()
     return render(request, 'templates/sign_up.html', {'form':form})
 
  
