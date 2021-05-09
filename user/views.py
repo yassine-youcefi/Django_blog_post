be20767d244258e5053ed0a9b3ed2a3fb3cbc778
yani27 <yanilacamora@gmail.com>
2021-05-09 13:46:59 +0100

    add custum form

--- user/views.py
+++ user/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def sign_up(request):
-    if request.method == 'POST':    
-        form = UserCreationForm(request.POST)
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account is create successfuly for {username}')
+            messages.success(
+                request, f'Account is create successfuly for {username}')
             return redirect('index')
     else:
-            form = UserCreationForm()
-    return render(request, 'templates/sign_up.html', {'form':form})
-
- 
+        form = UserRegisterForm()
+    return render(request, 'templates/sign_up.html', {'form': form})
