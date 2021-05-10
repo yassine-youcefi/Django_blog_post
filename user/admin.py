98af01f7e427966194293c44a96e378eb4c6434c
yasine-youcefi <yanilacamora@gmail.com>
2021-05-10 14:58:05 +0100

    add profile model

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from user.models import Profile
 
-# Register your models here.
+admin.site.register(Profile)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100, default="")
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    image = models.ImageField(upload_to='profile_pics', default='default.jpg')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
