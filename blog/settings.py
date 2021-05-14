965bebdf70411491fc8ceee67a8e7b886682665f
yasine-youcefi <yanilacamora@gmail.com>
2021-05-14 18:10:57 +0100

    fix media and css linginf

--- blog/settings.py
+++ blog/settings.py
@@ -110,8 +110,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
