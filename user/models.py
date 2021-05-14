71d5e715bd74cb74377b1d3f9a89cd76480adc54
yasine-youcefi <yanilacamora@gmail.com>
2021-05-14 19:45:06 +0100

    add signals for profile creation & update profile  model

--- user/models.py
+++ user/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=True, null=True)
     address = models.CharField(max_length=100, default="")
+    age = models.IntegerField(null=True, blank=True)
     GENDER_MALE = 0
     GENDER_FEMALE = 1
     GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
-    gender = models.IntegerField(choices=GENDER_CHOICES)
+    gender = models.IntegerField(choices=GENDER_CHOICES, null=True, blank=True)
     image = models.ImageField(upload_to='profile_pics', default='default.jpg')
     # image = models.FileField()
 
