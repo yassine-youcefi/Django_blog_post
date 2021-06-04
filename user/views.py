59b492e2e8b9a30eea254fadb092e8d1ee6b39e2
yasine-youcefi <yanilacamora@gmail.com>
2021-06-04 23:17:44 +0100

    feat : add user and profile forms validators

--- user/views.py
+++ user/views.py
@@ -21,8 +21,19 @@ def sign_up(request):
 
 @login_required
 def profile(request):
-    user_form = UserUpdateForm()
-    profile_form = ProfileUpdateForm()
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your account was updated')
+            return redirect('profile')
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'user_form': user_form,
