70b218ba0dcef4c79f196b1462210adff49e529c
yani <mohamed.youcefi.etu@univ-mosta.dz>
2021-08-11 22:47:20 +0100

    update

--- yani_blog/urls.py
+++ yani_blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
+from .views import PostListView
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('about/', views.about, name='about')
 ]
--- yani_blog/views.py
+++ yani_blog/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post,User
-
-
+from django.views.generic import ListView
 
 
 
@@ -13,6 +12,9 @@ def index(request):
 
     }
     return render(request, 'templates/index.html', context)
+
+class PostListView():
+    model = Post 
 
  
 def about(request):
